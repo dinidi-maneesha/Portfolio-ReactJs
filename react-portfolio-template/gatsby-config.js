5ee3be3f79381389bad047b78565ba293b4d748d
dnidimaneesha <96563@fhss.sjp.ac.lk>
2022-03-07 01:28:54 +0530

    modified tab bar

--- react-portfolio-template/gatsby-config.js
+++ react-portfolio-template/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Dinidi Maneesha`,
+    title: `Profile page`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
