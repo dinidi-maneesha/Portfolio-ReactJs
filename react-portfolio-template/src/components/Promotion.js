05536fb4668f2a4bb4ac071143e82e8e45a8ea94
dnidimaneesha <96563@fhss.sjp.ac.lk>
2022-03-08 00:10:51 +0530

    finish all changes

--- react-portfolio-template/src/components/Promotion.js
+++ react-portfolio-template/src/components/Promotion.js
@@ -10,7 +10,14 @@ const Promotion = () => {
           <Fade bottom cascade>
             <h1>{data.promotionHeading}</h1>
           </Fade>
+          <h2>{data.promotionHeading2}</h2>
           <p>{data.promotionPara}</p>
+          <h2>{data.promotionHeading3}</h2>
+          <p>{data.promotionPara2}</p>
+          <h2>{data.promotionHeading4}</h2>
+          <p>{data.promotionPara3}</p>
+          <h2>{data.promotionHeading5}</h2>
+          <p>{data.promotionPara4}</p>
         </div>
       </div>
     </div>
